0d7ce0eb1df17cb0f407f1796a23b54b086ab061
JoshBone <jozsef.bone@gmail.com>
2019-07-26 16:37:41 +0200

    fix(config): Moving config files to their separate directory.

---
+++ src/config/config-menu.js
@@ -0,0 +1,13 @@
+const configMenu = [
+  {name: 'Dashboard', icon: 'dashboard'},
+  {name: 'Accession', icon: 'swap'},
+  {name: 'Archival Unit', icon: 'apartment'},
+  {name: 'ISAAR-CPF', icon: 'team'},
+  {name: 'ISAD(G)', icon: 'profile'},
+  {name: 'Finding Aids', icon: 'file'},
+  {name: 'Lists', icon: 'unordered-list'},
+  {name: 'MLR', icon: 'inbox'},
+  {name: 'Digitization', icon: 'desktop'},
+];
+
+export default configMenu;
